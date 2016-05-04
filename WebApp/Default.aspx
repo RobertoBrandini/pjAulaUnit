bbcc0c9f2da8462f1c349a0e3bda05c9ef8e2572
robertobrandini <dp.sistemas@gmail.com>
2016-05-03 22:10:09 -0300

    Web APP Funcionando

---
+++ WebApp/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="Salario Atual"></asp:Label>
+        <asp:TextBox ID="txtSalarioAtual" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Aumento"></asp:Label>
+        <asp:TextBox ID="txtAumento" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblResposta" runat="server" Text="-"></asp:Label>
+        <br />
+        <asp:Label ID="lblResultado2" runat="server" Text="-"></asp:Label>
+        <br />
+
+        <asp:Button ID="btnCalcularSalario" runat="server" OnClick="btnCalcularSalario_Click" Text="Calcular Novo Salario" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApp/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalcularSalario_Click(object sender, EventArgs e)
+        {
+            Decimal salarioT = 0.00m;
+            Decimal.TryParse(txtSalarioAtual.Text, out salarioT);
+            if (salarioT == 0)
+            {
+                lblResposta.Text = "RN.SAL.001 - Informe o Salario";
+                return;
+            }
+
+            Decimal SalarioAtual = Convert.ToDecimal(txtSalarioAtual.Text);
+            Decimal Aumento = Convert.ToDecimal(txtAumento.Text);
+
+            Decimal NovoSalario = Negocio.Calculo.AumentoSalario(SalarioAtual, Aumento);
+            Decimal NovoSalario2 = Negocio.Calculo.AumentoSalario(SalarioAtual);
+
+            lblResposta.Text = String.Format("Novo Salario : {0} ", NovoSalario);
+            lblResultado2.Text = String.Format("Novo Salario : {0} ", NovoSalario2);
+            //lblResposta.Text = "Novo Salario : " + NovoSalario.ToString();
+
+        }
+    }
+}
