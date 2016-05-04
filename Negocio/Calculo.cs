5e5acb0db899feff2fe68db3cfdb2f6ddd5d65e5
robertobrandini <dp.sistemas@gmail.com>
2016-05-03 21:30:31 -0300

    Documentacao basica

--- Negocio/Calculo.cs
+++ Negocio/Calculo.cs
@@ -9,11 +9,22 @@ namespace Negocio
     public static class Calculo
     {
 
+        /// <summary>
+        /// Aumento Salario com percentuals
+        /// </summary>
+        /// <param name="SalarioAtual"></param>
+        /// <param name="PercentualAumento"></param>
+        /// <returns></returns>
         public static Decimal AumentoSalario(Decimal SalarioAtual, Decimal PercentualAumento)
         {
             return SalarioAtual * (1 + (PercentualAumento/100));
         }
 
+        /// <summary>
+        /// Aumento Salario padrao
+        /// </summary>
+        /// <param name="SalarioAtual"></param>
+        /// <returns></returns>
         public static Decimal AumentoSalario(Decimal SalarioAtual)
         {
             return SalarioAtual * (1.20m);
